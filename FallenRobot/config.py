185d979fc8355e7a1ed2a0fe9a4ad13991e747ea
Zfini <161265506+Zfini@users.noreply.github.com>
2024-12-02 17:57:02 +0530

    Update config.py

--- FallenRobot/config.py
+++ FallenRobot/config.py
@@ -14,13 +14,13 @@ class Config(object):
     MONGO_DB_URI = ""  # Get ths value from cloud.mongodb.com
 
     # Telegraph link of the image which will be shown at start command.
-    START_IMG = "https://files.catbox.moe/pausrx.jpg"
+    START_IMG = "https://envs.sh/Yx1.jpg"
 
     DB_NAME = "Hanabi"
 
     SUPPORT_CHAT = "lolpagalokigc"  # Your Telegram support group chat username where your users will go and bother you
 
-    TOKEN = ""  # Get bot token from @BotFather on Telegram
+    TOKEN = "6769807249:AAHJoA2oMBk5UUETGiIE3KHY69EfH3kqoO0"  # Get bot token from @BotFather on Telegram
 
     TIME_API_KEY = ""  # Get this value from https://timezonedb.com/api
 
@@ -30,7 +30,7 @@ class Config(object):
 
     # Optional fields
     BL_CHATS = []  # List of groups that you want blacklisted.
-    DRAGONS = [7526369190]  # User id of sudo users
+    DRAGONS = [5268691896]  # User id of sudo users
     DEV_USERS = [5268691896]  # User id of dev users
     DEMONS = [5268691896]  # User id of support users
     TIGERS = [5268691896]  # User id of tiger users
